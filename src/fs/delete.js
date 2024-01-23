d25c8bf51d7f4d5fe214bef56701e0c9a0c4f31e
Duxcoder <iinegve_25@mail.ru>
2024-01-23 22:02:16 +0300

    feat: complete delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { rm } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+    await rm(filePath);
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
