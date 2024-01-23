a6adf048634fc8a20dbff2d9f9113b935082f128
Duxcoder <iinegve_25@mail.ru>
2024-01-23 22:13:03 +0300

    fix: complete rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const oldName = join(__dirname, 'files', 'wrongFilename.txt');
+    const newName = join(__dirname, 'files', 'properFilename.md');
+    await fs.rename(oldName, newName);
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
