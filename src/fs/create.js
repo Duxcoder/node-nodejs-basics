3fec896a8a1447e4306852df4aeff4a8ce6a2a89
Duxcoder <iinegve_25@mail.ru>
2024-01-23 20:30:22 +0300

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { open } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+    const fileHandle = await open(filePath, 'wx');
+    await fileHandle.writeFile('I am fresh and young');
+    await fileHandle.close();
+  } catch (e) {
+    console.log('file exist:', e.message);
+  }
 };
 
 await create();
