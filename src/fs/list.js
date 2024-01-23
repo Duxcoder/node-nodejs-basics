65e66ab38b764831291409612518ca82987a1add
Duxcoder <iinegve_25@mail.ru>
2024-01-23 22:08:32 +0300

    feat: complete list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const dirPath = join(__dirname, 'files');
+    console.log(await readdir(dirPath));
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
