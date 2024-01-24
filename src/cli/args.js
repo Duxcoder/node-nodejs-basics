a73e6e63b89c57ecbc45bcf860840eaa5deeb177
Duxcoder <iinegve_25@mail.ru>
2024-01-24 19:24:24 +0300

    feat: complete args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+const createPrettyFormat = (val, i) => `${val[i].slice(2)} is ${val[i + 1]}`;
+
 const parseArgs = () => {
-    // Write your code here 
+  const PREFIX = '--';
+  const result = [];
+  const { argv } = process;
+  for (let i = 0; i < argv.length - 1; i++) {
+    if (argv[i].startsWith(PREFIX)) result.push(createPrettyFormat(argv, i));
+  }
+  console.log(result.join(', '));
 };
 
 parseArgs();
