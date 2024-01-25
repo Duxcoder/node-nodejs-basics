f7cbb8cf459fb988b85bff2d0c545b572bdd8b45
Duxcoder <iinegve_25@mail.ru>
2024-01-25 19:41:00 +0300

    feat: complete streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import * as process from 'node:process';
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filename = join(__dirname, 'files', 'fileToRead.txt');
+
+  const fileStream = createReadStream(filename);
+
+  fileStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
