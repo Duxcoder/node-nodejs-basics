aa38b457f81f039fa8d126b484b9cca0183c42db
Duxcoder <iinegve_25@mail.ru>
2024-01-27 22:47:12 +0300

    feat: complete streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as process from 'node:process';
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filename = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeStream = createWriteStream(filename);
+
+  process.stdin.pipe(writeStream);
+  writeStream.on('open', () => console.log(`Send text to fileToWrite:`));
 };
 
 await write();
